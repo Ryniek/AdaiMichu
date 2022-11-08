205ee5776896c1113dbd229fe346b70e4f7046ac
Michał Ryński <michalrynski96@gmail.com>
2022-11-08 18:21:17 +0100

    Implementing OAuth2 Jwt resource server

--- src/main/java/pl/rynski/adaimichal/configuration/SecurityConfig.java
+++ src/main/java/pl/rynski/adaimichal/configuration/SecurityConfig.java
@@ -1,17 +1,23 @@
 package pl.rynski.adaimichal.configuration;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.server.resource.web.BearerTokenAuthenticationEntryPoint;
+import org.springframework.security.oauth2.server.resource.web.access.BearerTokenAccessDeniedHandler;
 import org.springframework.security.web.SecurityFilterChain;
 
 import lombok.RequiredArgsConstructor;
 import pl.rynski.adaimichal.security.CustomUserDetailsService;
 
 @Configuration
+@EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 	
@@ -24,14 +30,18 @@ public class SecurityConfig {
 	
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http.userDetailsService(customUserDetailsService);
 		
-		http.cors().and().csrf().disable()
-		.authorizeRequests((autz) -> autz.anyRequest().authenticated())
-		.httpBasic().and()
-		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		http
+		.cors().and()
+		.csrf().disable()
+		.authorizeHttpRequests((autz) -> autz.anyRequest().authenticated())
+		.httpBasic().disable()
+		.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+		.exceptionHandling((exceptions) -> exceptions
+				.authenticationEntryPoint(new BearerTokenAuthenticationEntryPoint())
+				.accessDeniedHandler(new BearerTokenAccessDeniedHandler()));
 		
 		return http.build();
 	}
-	
 }
