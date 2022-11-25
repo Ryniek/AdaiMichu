9c224d837f3e16f0ac73a36bc1f537a0147a7c4b
Michał Ryński <michalrynski96@gmail.com>
2022-11-25 16:44:10 +0100

    Enabled cross origin

--- src/main/java/pl/rynski/adaimichal/controller/AuthController.java
+++ src/main/java/pl/rynski/adaimichal/controller/AuthController.java
@@ -1,6 +1,7 @@
 package pl.rynski.adaimichal.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import pl.rynski.adaimichal.dao.dto.request.UserDto;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
--- src/main/java/pl/rynski/adaimichal/controller/TaskController.java
+++ src/main/java/pl/rynski/adaimichal/controller/TaskController.java
@@ -4,6 +4,7 @@ import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import lombok.RequiredArgsConstructor;
 import pl.rynski.adaimichal.dao.dto.request.TaskDto;
 import pl.rynski.adaimichal.service.TaskService;
 
+@CrossOrigin("*")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/tasks")
